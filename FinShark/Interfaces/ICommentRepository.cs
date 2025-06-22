61c59b86a8bd5f2bd235af0819e769e32aaaba79
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-23 02:17:45 +0300

    Interfaces

--- FinShark/Interfaces/ICommentRepository.cs
+++ FinShark/Interfaces/ICommentRepository.cs
@@ -1,10 +1,11 @@
-﻿using FinShark.Models;
+﻿using FinShark.Helper;
+using FinShark.Models;
 
 namespace FinShark.Interfaces
 {
     public interface ICommentRepository
     {
-        Task<List<Comment>> GetAllAsync();
+        Task<List<Comment>> GetAllAsync(CommentQueryObject queryObject);
         Task<Comment?> GetByIdAsync(int id);
         Task<Comment>CreateAsync(Comment commentModel);
         Task<Comment?> UpdateAsync(int id , Comment commentModel);
