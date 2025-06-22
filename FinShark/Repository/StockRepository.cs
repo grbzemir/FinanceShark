a94123aaf3c83ccab04226ffb51f4b8c7b02a0b9
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-23 00:59:57 +0300

    RepositoryBase

--- FinShark/Repository/StockRepository.cs
+++ FinShark/Repository/StockRepository.cs
@@ -74,6 +74,11 @@ namespace FinShark.Repository
                                  .FirstOrDefaultAsync(i => i.Id == id);
         }
 
+        public async Task<Stock?> GetBySymbolAsync(string symbol)
+        {
+            return await _context.Stocks.FirstOrDefaultAsync(s => s.Symbol == symbol);
+        }
+
         public Task<bool> StockExists(int id)
         {
             return _context.Stocks.AnyAsync(s => s.Id == id);
