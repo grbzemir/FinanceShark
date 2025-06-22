910bad30bf2b73f8f18322e6397f421e4686e4ee
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-23 00:59:36 +0300

    Interfaces

---
+++ FinShark/Interfaces/IPortfolioRepository.cs
@@ -0,0 +1,10 @@
+﻿using FinShark.Models;
+
+namespace FinShark.Interfaces
+{
+    public interface IPortfolioRepository
+    {
+        Task<List<Stock>> GetUserPortfolio(AppUser user);
+        Task<Portfolio>CreateAsync(Portfolio portfolio);
+    }
+}
